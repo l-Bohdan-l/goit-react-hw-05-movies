e528205d6ca2d3643b48b0f0f89a391cda62a5c3
l-Bohdan-l <bpapushoj@gmail.com>
2022-01-14 21:22:14 +0200

    create components js files (empty)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import './App.scss';
 import React from 'react';
-import { Searchbar } from './components/Searchbar/Searchbar';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { ImageGallery } from './components/ImageGallery/ImageGallery';
+
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 import { useState, useEffect } from 'react';
+
+import { HomePage } from './components/HomePage/HomePage';
 
 // key = 0e4aaee08aabcf1cd893aec1f6e895b9
 
-function App() {
-  const [modalContent, setModalContent] = useState('');
-  const [searchQuery, setSearchQuery] = useState('');
-  const [page, setPage] = useState(1);
-
-  const handleSubmit = imgQuery => {
-    setSearchQuery(imgQuery);
-  };
-
+function App() {  
+  
   return (
     <div className="App">
-      <Searchbar onSubmit={handleSubmit} />
-      <ToastContainer />
-      <ImageGallery value={searchQuery} />
+      <HomePage/>
     </div>
   );
 }
